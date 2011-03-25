7070758dc823eddc82cf4dc151f9b2d69e66bf79
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-03-25 17:15:16 +0100

    clean up code

--- lib/s_compare.rb
+++ lib/s_compare.rb
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
 
 require "ripper"
-require_relative './s_compare/simplifier.rb'
-require_relative './s_compare/comparator.rb'
+require_relative 's_compare/simplifier.rb'
+require_relative 's_compare/comparator.rb'
